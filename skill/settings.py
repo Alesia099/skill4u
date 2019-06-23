cc426f73a298834751faa333f5537632c155d0d9
M11N3 <stronger0804@mail.com>
2019-06-23 16:39:13 +0300

    Deploy

--- skill/settings.py
+++ skill/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +29,7 @@ SECRET_KEY = '9bjawq^4hp0^pym#&7gkygr!1j%06oir2e#508r=cj$95&fs=j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'registration.User'
 
